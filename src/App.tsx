785fe5955af52b2aec2629b257760e8874795d80
VELASTEGUI LOPEZ RICARDO <rvelastegui@sasf.local>
2024-10-04 17:58:27 -0500

    segundo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 
 import Card, {CardBody} from './components/Card'; 
 import List from './components/List';
+import Button from './components/Button';
+import { useState } from 'react';
  //debo importar con llaves cuando no es el componente original
 function App(){
+
+const [isLoading, setIsLoading]= useState(false);
+const handleClick = () => setIsLoading(!isLoading);
+
   //jsx  ->  react.createlement
-  const list: string[] = [];
+  const list: string[] = ['ejemplo1','ejemplo2','ejemplo3'];
+  
   const handleSelect = (elemento:string) => {
     console.log("imprimiendo",elemento);
   };
@@ -13,6 +20,7 @@ function App(){
   return <Card>
     <CardBody  title='hola gente' text='este es el texto'/>
     {contenido}    
+    <Button isLoading={isLoading} onClick={handleClick} children={undefined} ></Button>
     </Card>;
 }
 
---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+
+type Props = {
+    children: ReactNode;
+    isLoading: boolean;
+    onClick: () => void;
+}
+
+function Button({children, isLoading, onClick}:Props){
+    return (
+        <button onClick = {onClick} disabled={isLoading} type="button" className={`btn btn-${isLoading ? 'secondary': 'primary'}`}>
+
+            {isLoading? 'cargando...': children}</button>
+    );
+}
+
+export default Button; 
